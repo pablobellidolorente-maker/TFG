5c0d2b9c600c58be7bdf30b7979294699c112829
pablobellidolorente-maker <pablo.bellidolorente@gmail.com>
2026-03-16 12:23:44 +0100

    refinando y comentando

--- indice.ps1
+++ indice.ps1
@@ -1,3 +1,6 @@
+<#Código básico del menu, en el que se le dan las opciones a elegir al usuario, que,
+llamaran a los demas scripts para ejecutar la configuracion elegida#>
+
 do {
 Write-Host ""
 Write-Host "========BIENVENIDO========"
@@ -21,9 +24,8 @@ switch ($respuesta) {
     "2" {Write-Host "rutascript2"}
     "3" {Write-Host "rutascript3"}
     "4" {Write-Host "rutascript4"}
-    "5" {Write-Host "Saliendo, gracias..."
-    break}
-    default {Write-Host "Opción no válida"}
+    "5" {Write-Host "Saliendo, gracias..."}
+    default {Write-Host "Opción no válida, introduzca los parámetros: 1 , 2 , 3 , 4 o 5"}
 
-}
+}while ($respuesta -ne "5")
 
