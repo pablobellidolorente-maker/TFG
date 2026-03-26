80884837c99361a65f5ebfc57dd86ef7b5b4a79b
pablobellidolorente-maker <pablo.bellidolorente@gmail.com>
2026-03-26 12:51:11 +0100

    CREACION SCRIPTS USUARIOS Y CARPETAS

---
+++ modulos/configuracion basica/estructura_carpetas.ps1
@@ -0,0 +1,29 @@
+#Crearemos las carpetas necesarias para una empresa promedio, y así despues dar permisos a los usuarios pertinentes
+
+
+Write-Host "========= CREACIÓN DE ESTRUCTURA DE CARPETAS ========="
+
+#Array lista de carpetas a crear
+
+$carpetas = @(
+    "C:\Datos",
+    "C:\Proyectos",
+    "C:\Departamentos",
+    "C:\UsuariosEmpresa",
+    "C:\Temp",
+    "C:\Logs"
+)
+
+#Creacion de las carpetas y su comprobacion de si ya existen
+
+foreach ($carpeta in $carpetas) {
+    if (-not (Test-Path $carpeta)){
+        Write-Host "Creando carpeta $carpeta"
+        New-Item -ItemType Directory -Path $carpeta | Out-Null
+    } else {
+        Write-Host "La carpeta ya existe: $carpeta"    
+    }
+}
+Write-Host " "
+
+Write-Host "Estructura creada correctamente"
---
+++ modulos/configuracion basica/usuarios.ps1
@@ -0,0 +1,171 @@
+
+Write-Host "========= GESTIÓN DE USUARIOS Y GRUPOS ========="
+
+#Declaramos funciones para que así se las pueda llamar desde el menu interactivo creado abajo del todo
+
+#CREAR USUARIO
+
+function Crear-Usuario {
+    
+    Write-Host " "
+    Write-Host "--- Crear Usuario ---"
+
+    $nombre = Read-Host "Introduzca el nombre del usuario"
+
+    #Comprobacion de nombre no en blanco
+
+    if ([string]::IsNullOrWhiteSpace($nombre)) {
+        Write-Host "ERROR: El nombre no puede estar vacío"
+        return
+}
+
+#Comprobacion de si el nombre existe
+
+if (Get-LocalUser -Name $nombre -ErrorAction SilentrlyContinue){
+    Write-Host "ERROR: El usuario ya existe"
+    return
+}
+
+#Descripcion para el usuario (no es obligatoria)
+
+$descripcion = Read-Host "Descripción del usuario (puede dejarse en blanco)"
+
+#Pedir contraseña
+
+do {
+    $pass1 = Read-Host "Introduzca la contraseña" -AsSecureString
+    $pass2 = Read-Host "Repita la contraseña" -AsSecureString
+
+    #IMPORTANTE!!! Al convertir las contraseñas a SecureString, dejan de ser texto plano,
+    #Por ello las dos siguientes lineas lo transforman de nuevoa a texto plano para asi poder compararlas
+
+    $plain1 = [Runtime.InterpopServices.Marshall]::PtrToStringAuto ([Runtime.InterpopServices.Marshall]::SecureStringToBSTR(pass1))
+    $plain2 = [Runtime.InterpopServices.Marshall]::PtrToStringAuto ([Runtime.InterpopServices.Marshall]::SecureStringToBSTR(pass2))
+
+    if (plain1 -ne $plain2){
+        Write-Host "Las contraseñas no coinciden"
+    }
+
+} (while plain1 -ne plain2)
+
+#Al llegar aquí tenemos ya el nombre y las credenciales del usuario, ahora con esos datos en la variable, creamos el usuario
+
+New-LocalUser -Name $nombre -Password $pass1 -Description $descripcion
+Write-Host "Usuario '$nombre' creado correctamente"
+
+}
+
+#CREAR GRUPO
+
+function Crear-Grupo {
+
+    Write-Host " "
+    Write-Host "--- Crear Grupo ---"
+
+    $grupo = Read-Host "Introduzca el nombre del grupo"
+
+        #Comprobacion de nombre no en blanco
+
+    if ([string]::IsNullOrWhiteSpace($grupo)) {
+        Write-Host "ERROR: El nombre no puede estar vacío."
+        return
+    }
+
+        #Comprobacion de si el nombre existe
+
+    if (Get-LocalGroup -Name $grupo -ErrorAction SilentrlyContinue){
+        Write-Host "ERROR: El grupo '$grupo' ya existe"
+        return    
+    }
+    
+        #AQUI SE CREA YA EL GRUPO
+
+    New-LocalGroup -Name $grupo
+    Write-Host "Grupo '$grupo' creado correctamente"
+}
+
+#AÑADIR USUARIOS A UN GRUPO
+
+function Anadir-Usuario-Grupo{
+    Write-Host " "
+    Write-Host "--- Añadir usuario a grupo ---"
+
+
+    Write-Host " "
+    
+    $usuario = Read-Host "Nombre del usuario"
+    $grupo = Read-Host "Nombre del grupo"
+
+    #Al igual que antes, validamos si el usuario y grupo existen
+
+     if (-not (Get-LocalUser -Name $usuario -ErrorAction SilentlyContinue)) {
+        Write-Host "ERROR: El usuario '$usuario' no existe."
+        return
+    }
+
+    if (-not (Get-LocalGroup -Name $grupo -ErrorAction SilentlyContinue)) {
+        Write-Host "ERROR: El grupo '$grupo' no existe."
+        return
+    }
+
+    #Una vez se han realizado todas las comprobaciones, se añade el usuario al grupo
+
+    Add-LocalGroupMember -Group $grupo -Member $usuario -ErrorAction Stop
+    Write-Host "Usuario '$usuario' añadido al grupo '$grupo'."
+
+}
+
+function Ver-Usuarios {
+        #Las siguientes lineas hacen que el host vea que usuarios existen
+
+    Write-Host " "
+    
+    Write-Host "Usuarios disponibles:"
+    
+    Get-LocalUser | Format-Table Name, Enabled, Description
+
+}
+
+function Ver-Grupos {
+        #Las siguientes lineas hacen que el host vea que usuarios existen
+
+    Write-Host " "
+    
+    Write-Host "Grupos disponibles:"
+    
+    Get-LocalGroup | Format-Table Name
+}
+
+
+#=========== MENÚ QUE VERÁ EL USUARIO ============
+
+#Creamos un bucle para que siempre se vuelva al menu a no ser que se especifique "salir"
+
+do {
+    
+    Write-Host "`nSeleccione una opción"
+    Write-Host "1) Crear un usuario"
+    Write-Host "2) Crear un grupo"
+    Write-Host "3) Añadir un usuario a un grupo existente"
+    Write-Host "4) Ver usuarios existentes"
+    Write-Host "5) Ver grupos existentes"
+    Write-Host "6) Salir"
+
+    #variable de la respuesta
+
+    $opcion = Read-Host "Opción"
+
+    #Creamos un switch, que permite que un mismo input pueda tener varios valores (las opciones)
+
+    switch ($opcion) {
+        "1" { Crear-Usuario }
+        "2" { Crear-Grupo }
+        "3" { Anadir_Usuario-Grupo }
+        "4" { Ver-Usuarios }
+        "5" { Ver-Grupos }
+        "6" { Write-Host "Saliendo del menu de usuarios"}
+
+        default { Write-Host "ERROR: Recuerde, debe de elegir un valor entre (1-6)"}    
+    }
+
+} while ($opcion -ne "6")
