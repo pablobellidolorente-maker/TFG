59dfecae7c3119be9a9a83f4169aa9f6d3d6620d
pablobellidolorente-maker <pablo.bellidolorente@gmail.com>
2026-05-04 10:30:34 +0200

    M

--- modulos/seguridad/Mseguridad.ps1
+++ modulos/seguridad/Mseguridad.ps1
@@ -11,3 +11,36 @@
 
 4) HARDENING.ps1
 #>
+
+
+
+Write-Host "--------------INICIANDO CONFIGURACION SEGURIDAD--------------"
+
+#======================================================
+# 1.FIREWALL
+
+Write-Host "`nEjecutando firewall.ps1"
+& "$PSScriptRoot\firewall.ps1"
+
+#==============================================================
+# 2. DEFENDER
+
+Write-Host "`nEjecutando defender.ps1"
+& "$PSScriptRoot\defender.ps1"
+
+#===============================================================
+# 3. AUDITORIA
+
+Write-Host "`nEjecutando auditoria.ps1"
+& "$PSScriptRoot\auditoria.ps1"
+
+#================================================================
+# 4. HARDENING
+
+Write-Host "`nEjecutando hardening.ps1"
+& "$PSScriptRoot\hardening.ps1"
+
+
+
+Write-Host "`n================= CONFIGURACION SEGURIDAD COMPLETADA =================" -ForegroundColor Green
+
