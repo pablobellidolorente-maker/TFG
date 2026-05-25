8bc6d6c50b697490aae571a5561ed5b0b04b505c
alvarixto32-byte <alvarixto32@gmail.com>
2026-05-25 19:20:53 +0200

    Version Listo

--- modulos/configuracion basica/Mconf_basica.ps1
+++ modulos/configuracion basica/Mconf_basica.ps1
@@ -3,18 +3,18 @@
 
 #ORDEN DE EJECUCION OPTIMA DE LOS SCRIPTS
 
-1.estructura.ps1
+#1.estructura.ps1
 
-2.usuarios.ps1
+#2.usuarios.ps1
 
-3.permisos.ps1
+#3.permisos.ps1
 
-4.hostname.ps1
+#4.hostname.ps1
 
-5.red.ps1
+#5.red.ps1
 
-6.sistema.ps1x 
-#>
+#6.sistema.ps1x 
+
 
 # ============================
 #   IMPORTAR MODULOS DE LOGGING
